66469c46c876e5435350be782f81d079b48b0a2b
Aarow <aarow0021@gmail.com>
2024-12-16 07:40:06 -0800

    Continued customization

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 window.onload = function() {
 
-    let dropdowns = document.querySelectorAll("button:has(~ ul.dropdown)")
+    let body = document.getElementsByName(body)[0];
+
+    let dropdowns = document.querySelectorAll("button:has(~ ul.dropdown)");
 
     dropdowns.forEach( function(elem) {
         elem.addEventListener("mouseleave", unfocus);
     })
+
+    if (body.id = "customize") {
+        let custom_exterior_buttons = document.querySelectorAll("product-nav input");
+        let car = document.getElementsByClassName("customization-exterior-car")[0];
+    
+        custom_exterior_buttons.forEach( function(elem) {
+            elem.addEventListener("change", checkExteriorButtons);
+        })
+
+        function checkExteriorButtons (event) {
+            let button = event.target;
+            if (button.checked) {
+                car.classList += button.value;
+            }
+        }
+    }
     
     function unfocus(event) {
         event.target.blur();
     }
+
 }
