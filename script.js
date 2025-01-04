67a945c91c7d008978e004b14610ec9ae777fa49
Aarow <aarow0021@gmail.com>
2025-01-03 22:13:58 -0800

    Scroll buttons

--- script.js
+++ script.js
@@ -8,16 +8,39 @@ window.onload = function() {
         elem.addEventListener("mouseleave", unfocus);
     })
 
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function scroll(element, amount) {
+        for (let i = 0; i < 20; i++) {
+            element.scrollLeft += amount/20;
+            await sleep(10);
+        }
+    }
+
     if (body.id == "customize") {
         let custom_exterior_buttons = document.querySelectorAll(".color-select input");
+        let custom_exterior_labels = document.querySelectorAll(".color-select label");
         let car = document.getElementsByClassName("customization-exterior-car")[0];
+        car.classList.add(localStorage.getItem("exterior_color"));
+
+        if (localStorage.getItem("exterior_button")) {
+            let activeLabel = document.querySelector(`[for=${localStorage.getItem("exterior_button")}]`);
+            activeLabel.classList.add("active");
+        }
     
         custom_exterior_buttons.forEach( function(elem) {
-            elem.addEventListener("change", checkExteriorButtons);
+            elem.addEventListener("change", checkCustomizerButtons);
         })
 
-        function checkExteriorButtons (event) {
+        function checkCustomizerButtons (event) {
             let button = event.target;
+            custom_exterior_labels.forEach( function(label) {
+                label.classList.remove("active");
+            })
+            let label = document.querySelector(`[for=${button.id}]`);
+            label.classList.toggle("active");
             car.classList.forEach(function(classItem) {
                 if (classItem.includes("custom-color")) {
                     car.classList.remove(classItem)
@@ -26,17 +49,41 @@ window.onload = function() {
             if (button.checked) {
                 car.classList.add(button.value);
                 localStorage.setItem("exterior_color", button.value);
+                localStorage.setItem("exterior_button", button.id);
             }
         }
     }
 
-    if (body.id == "settings" || body.id == "customize") {
+    if (body.id == "settings") {
         let car = document.getElementsByClassName("customization-exterior-car")[0];
         car.classList.add(localStorage.getItem("exterior_color"));
     }
     
     function unfocus(event) {
         event.target.blur();
+    }
+
+    if (document.querySelector(".scroll-arrow")) {
+        const scroll_parents = document.querySelectorAll(":has(> .scroll-arrow)");
+        scroll_parents.forEach(function(scroll_parent) {
+
+            const content = scroll_parent.querySelector(".content");
+            if (content.scrollWidth > content.offsetWidth) {
+                const arrow_left = scroll_parent.querySelector(".button-left");
+                const arrow_right = scroll_parent.querySelector(".button-right");
+                arrow_left.classList.remove("hidden");
+                arrow_right.classList.remove("hidden");
+
+                arrow_left.addEventListener("click", function(event) {
+                    scroll(content, -200);
+                })
+                arrow_right.addEventListener("click", function(event) {
+                    scroll(content, 200);
+                })
+            }
+
+
+        })
     }
 
 }
